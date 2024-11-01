ab5c713d7e4f0cf081889311923545660717fb36
cillian <cillian@otainsight.com>
2024-11-01 18:05:38 +0100

    extract login

---
+++ app/components/Login.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { auth, signOut, signIn } from "@/auth";
+import Link from "next/link";
+
+export const Login = async () => {
+  const session = await auth();
+
+  return (
+    <div className="flex items-center gap-5">
+      {session && session.user ? (
+        <>
+          <Link href="/startup/create">
+            <span>Create</span>
+          </Link>
+
+          <form
+            action={async () => {
+              "use server";
+
+              await signOut({ redirectTo: "/" });
+            }}
+          >
+            <button type="submit">Signout</button>
+          </form>
+          <Link href={`/user/${session.user.id}`}>
+            <span>{session?.user.name}</span>
+          </Link>
+        </>
+      ) : (
+        <form
+          action={async () => {
+            "use server";
+
+            await signIn("github");
+          }}
+        >
+          <button type="submit">Login</button>
+        </form>
+      )}
+    </div>
+  );
+};
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,9 @@
 import Link from "next/link";
 import Image from "next/image";
 import React from "react";
-import { auth, signOut, signIn } from "@/auth";
+import { Login } from "@/app/components/Login";
 
 export const Navbar = async () => {
-  const session = await auth();
 
   return (
     <header className="px-5 py-3 bj-white shadow-sm font-work-sans">
@@ -13,38 +12,8 @@ export const Navbar = async () => {
           <Image src="/logo.png" alt="logo" width={144} height={30} />
         </Link>
 
-        <div className="flex items-center gap-5">
-          {session && session.user ? (
-            <>
-              <Link href="/startup/create">
-                <span>Create</span>
-              </Link>
+          <Login />
 
-              <form
-                action={async () => {
-                  "use server";
-
-                  await signOut();
-                }}
-              >
-                <button type="submit">Signout</button>
-              </form>
-              <Link href={`/user/${session.user.id}`}>
-                <span>{session?.user.name}</span>
-              </Link>
-            </>
-          ) : (
-            <form
-              action={async () => {
-                "use server";
-
-                await signIn("github");
-              }}
-            >
-              <button type="submit">Login</button>
-            </form>
-          )}
-        </div>
       </nav>
     </header>
   );
