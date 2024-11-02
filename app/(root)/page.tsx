69acf2c8a7f115ac7b9263821a91c04eaa3821a9
cillian <cillian@otainsight.com>
2024-11-02 20:36:34 +0100

    add basic form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,12 @@
+import { Search } from "@/app/components/Search";
+
 export default function Home() {
   return (
     <>
       <div className="pink_container">
         <h1 className="heading">Home</h1>
+
+        <Search />
       </div>
     </>
   );
