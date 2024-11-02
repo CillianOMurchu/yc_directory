d9f9e5b7e9770edddf2ac0b97a3b474aa79a6b1f
cillian <cillian@otainsight.com>
2024-11-02 20:07:43 +0100

    set favicon

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl">Home</h1>
+      <div className="pink_container">
+        <h1 className="heading">Home</h1>
+      </div>
     </>
   );
 }
