96b52e87ad9166390167e123b27eb6b53012a894
cillian <cillian@otainsight.com>
2024-11-02 20:45:23 +0100

    fix

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,11 @@
 import { Search } from "@/app/components/Search";
 
-export default function Home({searchParams:Promise<{query:string}>}) {
-  const { query } = await searchParams.query;
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query: string }>;
+}) {
+  const query = (await searchParams).query;
   return (
     <>
       <div className="pink_container">
