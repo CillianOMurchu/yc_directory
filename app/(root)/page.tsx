378c274b54a0935d99769ecebdae31465527ab29
cillian <cillian@otainsight.com>
2024-11-13 23:39:30 +0100

    ignore 1password

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,15 @@
 import ChatBox from "@/app/components/Chat";
+import PromptForm from "@/app/components/PromptForm";
 import { auth } from "@/auth";
 
 const App = async () => {
   const session = await auth();
-  return <ChatBox session={session} />;
+  return (
+    <div className="app">
+      <ChatBox session={session} />
+      <PromptForm />
+    </div>
+  );
 };
 
 export default App;
---
+++ app/components/PromptForm.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import DialogPrompt from "@/app/components/promptForm/Dialog";
+
+type PromptFormProps = {};
+
+const PromptForm = () => {
+  return <DialogPrompt />;
+};
+
+export default PromptForm;
