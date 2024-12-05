37b94a4eab4384bc63b5ef52cc2f1d51bb12c22f
cillian <cillian@otainsight.com>
2024-12-05 19:42:36 +0100

    visible salesPlayground

--- app/api/movies/route.ts
+++ app/api/movies/route.ts
@@ -5,13 +5,8 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const client = await clientPromise;
     const db = client.db("mongodbVSCodePlaygroundDB");
-    const movies = await db
-      .collection("sales")
-      // .find({})
-      // .sort({ metacritic: -1 })
-      // .limit(10)
-      // .toArray();
-    // res.json(movies);
+    const movies = await db.collection("sales");
+    res.json(movies);
   } catch (e) {
     console.error(e);
   }
